454e2d8e70919f6a897d310bc388ce90af035477
Sojib81 <sojibten4412@gmail.com>
2024-08-14 13:21:39 +0600

    double vs tripple equal

---
+++ double-vs-tripple-equal.js
@@ -0,0 +1,10 @@
+//double equal check only the value(double equal will convert the type and then it will compare between the values)
+//triple equal check value and type
+
+const first = 1;
+const second = false;
+if (first == second) {
+  console.log("Condition is true");
+} else {
+  console.log("Condition is False");
+}
